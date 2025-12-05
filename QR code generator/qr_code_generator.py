e93738d33b0c7ca946e003e886f9b5a76f034ea3
Laurentiu Scutariu <laurentiu.scutariu@cs-at.com>
2025-12-05 11:36:22 +0100

    qr code generator made with a qr code png(which contains a cool website link) ;). And some info about it.

--- QR code generator/qr_code_generator.py
+++ QR code generator/qr_code_generator.py
@@ -1,10 +1,8 @@
 import qrcode
 
-data = input("Enter the text or URL: ").strip()
-filename = input("Enter the filename: ").strip()
+filename = input("Enter file name: ").strip()
+searchdata = input("Enter the query: ").strip()
 
-qr = qrcode.QRCode(box_size=10, border=4)
-qr.add_data(data)
-image = qr.make_image(fill_color="black", back_color="white")
-image.save(filename)
-print(f"QR code saved as {filename} ")
+img = qrcode.make(searchdata)
+type(img)  # qrcode.image.pil.PilImage
+img.save(filename + ".png")
